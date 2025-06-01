8a4e2257044f38ea6a6194f1d835d509d264f2c1
akilesh <70111201+akileshthuniki@users.noreply.github.com>
2025-06-01 02:08:42 -0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  basePath: '/portfolio',
-  assetPrefix: '/portfolio/',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   images: {
-    unoptimized: true
-  }
+    unoptimized: true,
+  },
 }
 
 export default nextConfig
